2e3e9f637b87212d8c85e1c8a5d59bdfacc7a9cb
Anna Trushnikova <safo1402@mail.ru>
2019-09-09 12:59:54 -0700

    connection of readline-sync library

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node 
 console.log('Welcome to the Brain Games!');
+
+import { actual } from '~/projects/frontend-project-lvl1/src/index.js';
+
+console.log('actual');
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import readlineSync from 'readline-sync.js';
+
+export const actual = readlineSync.question('May I have your name: ');
